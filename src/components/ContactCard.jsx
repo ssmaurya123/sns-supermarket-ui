a1b4c8d589254be368fda0e819fca753ed6b92b3
Saurabh Singh Maurya <smaurya@aistrike.com>
2026-02-14 23:59:00 +0530

    style: Add border radius to ContactCard for improved aesthetics

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const ContactCard = ({ icon, title, description }) => {
     return (
-        <Card sx={{ width: "300px", mb: 4, display: "flex", flexDirection: "row", alignItems: "stretch", p: 0 }}>
+        <Card sx={{ width: "300px", mb: 4, display: "flex", flexDirection: "row", alignItems: "stretch", p: 0, borderRadius: "12px" }}>
             <CardMedia
                 sx={{
                     width: 56,
